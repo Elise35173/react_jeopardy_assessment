40748a3e94f60654397ab8d32d0d680da4290b54
Elise Cossairt <elise_cossairt@hotmail.comgit config --global user.name Eliserunclear--globalgit config user.email elise_cossairt@hotmail.com>
2020-08-18 16:08:00 -0500

    Finished initial Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
+import Welcome from './components/welcome/Welcome';
+import Clock from './components/clock/Clock';
+import Contact from './components/contact/Contact';
+import Jeopardy from './components/jeopardy/Jeopardy';
+import Navigation from './components/navigation/Navigation';
+import Page404 from './components/page404/Page404';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Switch>
+        <Route
+          exact 
+          path="/" 
+          render={(props) => <Welcome {...props} name="Elise" />} 
+        />
+        <Route 
+          exact
+          path="/welcome/:name"
+          render={(props) => <Welcome {...props} name={props.match.params.name} />}
+        />
+        <Route exact path="/clock" component= {Clock} />
+        <Route exact path="/contact" component= {Contact} />
+        <Route exact path="jeopardy" component= {Jeopardy} />
+        <Route component={Page404} />
+      </Switch>
     </div>
   );
 }
