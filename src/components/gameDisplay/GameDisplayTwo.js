c181f145695043848c3565224b1ece01c225bfc5
Elise Cossairt <elise_cossairt@hotmail.comgit config --global user.name Eliserunclear--globalgit config user.email elise_cossairt@hotmail.com>
2020-08-31 16:41:56 -0500

    fixed user and answer to not be case sensitive

---
+++ src/components/gameDisplay/GameDisplayTwo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function GameDisplayTwo (props) {
+    return (
+        <div className="GameDisplay">
+            <strong>User's Score: {props.score}</strong>
+            <br />
+            <strong>Question: {props.question}</strong>
+            <br />
+            <strong>Value: {props.value}</strong>
+            <br />
+            <strong>Category: {props.category}</strong>
+            <br />
+            <strong>Answer: {props.answer}</strong>
+            <br />
+            <form className="SubmitAnswer" onSubmit={props.handleSubmit}>
+                <div>
+                    <label 
+                    htmlFor="userAnswer">Your Answer:
+                    </label>
+                    <input 
+                    type="text" 
+                    name="userAnswer"
+                    value={props.userAnswer}
+                    onChange={props.handleChange}
+                    />
+                </div>
+                <button>Submit Answer!</button>
+            </form>
+        </div>
+    )
+}
+
+export default GameDisplayTwo;
--- src/components/jeopardy/Jeopardy.js
+++ src/components/jeopardy/Jeopardy.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import JeopardyService from "../../services/jeopardyService";
+import GameDisplayOne from '../gameDisplay/GameDisplayOne';
+import GameDisplayTwo from '../gameDisplay/GameDisplayTwo';
+import GameDisplayThree from '../gameDisplay/GameDisplayThree';
 
 class Jeopardy extends Component {
   constructor(props){
@@ -8,8 +11,49 @@ class Jeopardy extends Component {
 
     this.client = new JeopardyService();
     this.state = {
+      revealOne: false,
+      revealTwo: false,
+      revealThree: false,
       formData: {userAnswer: ""},
-      data: {
+      dataOne: {
+        "id": null,
+        "answer": "",
+        "question": "",
+        "value": null,
+        "airdate": "",
+        "created_at": "",
+        "updated_at": "",
+        "category_id": null,
+        "game_id": null,
+        "invalid_count": null,
+        "category": {
+          "id": null,
+          "title": "",
+          "created_at": "",
+          "updated_at": "",
+          "clues_count": null,
+        }
+      },
+      dataTwo: {
+        "id": null,
+        "answer": "",
+        "question": "",
+        "value": null,
+        "airdate": "",
+        "created_at": "",
+        "updated_at": "",
+        "category_id": null,
+        "game_id": null,
+        "invalid_count": null,
+        "category": {
+          "id": null,
+          "title": "",
+          "created_at": "",
+          "updated_at": "",
+          "clues_count": null,
+        }
+      },
+      dataThree: {
         "id": null,
         "answer": "",
         "question": "",
@@ -35,7 +79,9 @@ class Jeopardy extends Component {
   getNewQuestion() {
     return this.client.getQuestion().then(result => {
       this.setState({
-        data: result.data[0]
+        dataOne: result.data[0], 
+        dataTwo: result.data[1],
+        dataThree: result.data[2]
       })
     })
   }
@@ -50,17 +96,58 @@ class Jeopardy extends Component {
 
     this.setState({ formData })
   }
-  handleSubmit = (event) => {
+
+  handleSubmitOne = (event) => {
     event.preventDefault();
-    let value = Number(this.state.data.value)
-    if (this.state.formData.userAnswer === this.state.data.answer) {
+    let userAnswerOne = (this.state.formData.userAnswer).toLowerCase()
+    let dataAnswerOne = (this.state.dataOne.answer).toLowerCase()
+    let valueOne = Number(this.state.dataOne.value)
+    if (userAnswerOne === dataAnswerOne) {
       this.setState((state) => ({
-        score: state.score + value
+        score: state.score + valueOne
       }))
     }
-    else {
+    else if (userAnswerOne !== dataAnswerOne) {
       this.setState((state) => ({
-        score: state.score - value
+        score: state.score - valueOne
+      }))
+    }
+    this.resetForm()
+    this.getNewQuestion()
+  }
+
+  handleSubmitTwo = (event) => {
+    event.preventDefault();
+    let userAnswerTwo = (this.state.formData.userAnswer).toLowerCase()
+    let dataAnswerTwo = (this.state.dataTwo.answer).toLowerCase()
+    let valueTwo = Number(this.state.dataTwo.value)
+    if (userAnswerTwo === dataAnswerTwo) {
+      this.setState((state) => ({
+        score: state.score + valueTwo
+      }))
+    }
+    else if (userAnswerTwo !== dataAnswerTwo) {
+      this.setState((state) => ({
+        score: state.score - valueTwo
+      }))
+    }
+    this.resetForm()
+    this.getNewQuestion()
+  }
+
+  handleSubmitThree = (event) => {
+    event.preventDefault();
+    let userAnswerThree = (this.state.formData.userAnswer).toLowerCase()
+    let dataAnswerThree = (this.state.dataThree.answer).toLowerCase()
+    let valueThree = Number(this.state.dataThree.value)
+    if (userAnswerThree === dataAnswerThree) {
+      this.setState((state) => ({
+        score: state.score + valueThree
+      }))
+    }
+    else if (userAnswerThree !== dataAnswerThree) {
+      this.setState((state) => ({
+        score: state.score - valueThree
       }))
     }
     this.resetForm()
@@ -69,39 +156,95 @@ class Jeopardy extends Component {
 
   resetForm = (event) => {
       this.setState({
-          formData: {userAnswer: ""}
+          formData: {userAnswer: ""},
+          revealOne: false,
+          revealTwo: false,
+          revealThree: false,
       })
   }
 
+  revealQuestionOne = (event) => {
+    this.setState({
+      revealOne: true
+    })
+  }
+
+  revealQuestionTwo = (event) => {
+    this.setState({
+      revealTwo: true
+    })
+  }
+
+  revealQuestionThree = (event) => {
+    this.setState({
+      revealThree: true
+    })
+  }
+
   render() {
-    return (
-      <div>
-        <strong>User's Score: {this.state.score}</strong>
-        <br />
-        <strong>Question: {this.state.data.question}</strong>
-        <br />
-        <strong>Value: {this.state.data.value}</strong>
-        <br />
-        <strong>Category: {this.state.data.category.title}</strong>
-        <br />
-        <strong>Answer: {this.state.data.answer}</strong>
-        <br />
-        <form className="SubmitAnswer" onSubmit={this.handleSubmit}>
-          <div>
-            <label 
-              htmlFor="userAnswer">Your Answer:
-            </label>
-            <input 
-              type="text" 
-              name="userAnswer"
-              value={this.state.formData.userAnswer}
-              onChange={this.handleChange}
+    if(this.state.dataOne.category && this.state.dataTwo.category && this.state.dataThree.category) {
+      let categoryOne = <button onClick={this.revealQuestionOne}>{this.state.dataOne.category.title}</button>
+      let categoryTwo = <button onClick={this.revealQuestionTwo}>{this.state.dataTwo.category.title}</button>
+      let categoryThree = <button onClick={this.revealQuestionThree}>{this.state.dataThree.category.title}</button>
+      if (this.state.revealOne) {
+        return (
+          <div className="Jeopardy">
+            <GameDisplayOne
+              score={this.state.score}
+              question={this.state.dataOne.question}
+              value={this.state.dataOne.value}
+              category={this.state.dataOne.category.title}
+              answer={this.state.dataOne.answer}
+              handleSubmit={this.handleSubmitOne}
+              userAnswer={this.state.formData.userAnswer}
+              handleChange={this.handleChange}
             />
           </div>
-          <button>Submit Answer!</button>
-        </form>
-      </div>
-    );
+        )
+      }
+      if (this.state.revealTwo) {
+        return (
+          <div className="Jeopardy">
+            <GameDisplayTwo
+              score={this.state.score}
+              question={this.state.dataTwo.question}
+              value={this.state.dataTwo.value}
+              category={this.state.dataTwo.category.title}
+              answer={this.state.dataTwo.answer}
+              handleSubmit={this.handleSubmitTwo}
+              userAnswer={this.state.formData.userAnswer}
+              handleChange={this.handleChange}
+            />
+          </div>
+        )
+      }
+      if (this.state.revealThree) {
+        return (
+          <div className="Jeopardy">
+            <GameDisplayThree
+              score={this.state.score}
+              question={this.state.dataThree.question}
+              value={this.state.dataThree.value}
+              category={this.state.dataThree.category.title}
+              answer={this.state.dataThree.answer}
+              handleSubmit={this.handleSubmitThree}
+              userAnswer={this.state.formData.userAnswer}
+              handleChange={this.handleChange}
+            />
+          </div>
+        )
+      }
+      return (
+        <div className="Jeopardy">
+          {this.state.score}
+          {categoryOne}
+          <br />
+          {categoryTwo}
+          <br />
+          {categoryThree}
+        </div>
+      );
+    }
   }
 }
 
